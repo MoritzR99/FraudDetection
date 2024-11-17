c22c7f0ee17d9801279131898d2ba821e4db5158
MoritzR99 <moritz.rath@student.ie.edu>
2024-11-17 18:41:44 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -93,13 +93,13 @@ elif page_selection == "Exploratory Data Analysis":
     with col1:
         st.write("### Non-Fraud Amount")
         load_graph("non_fraud_amount.pkl")
-        
-        st.write("### Fraud Count")
-        load_graph("fraud_count.pkl")
 
-    with col2:
         st.write("### Non-Fraud Count")
         load_graph("non_fraud_count.pkl")
+
+    with col2:
+        st.write("### Fraud Count")
+        load_graph("fraud_count.pkl")
 
         st.write("### Fraud Amount")
         load_graph("fraud_amount.pkl")
